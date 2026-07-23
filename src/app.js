eefd10761f4489cd973cfe281e91a722d2330278
Navneet Kumar <navneet.tech06@gmail.com>
2026-07-23 19:07:14 +0530

    feat: update signup API to accept user data from request body

--- src/app.js
+++ src/app.js
@@ -3,23 +3,31 @@ require("dotenv").config();
 const express = require('express');
 const connectDB =  require('./config/database'); // Import the database connection
 const app = express();
-
 const User = require('./models/user'); // Import the User model
-app.post('/signup', async (req,res) => {
-    try {
-        const user = new User({
-        firstName: "Navneet",
-        lastName: "Yadav",
-        emailId: "navneet@example.com",
-        password: "123***"
-    });
-    await user.save()
 
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+app.post('/signup', async (req,res) => {
+    const user = new User(req.body);
+    
+try {
+    await user.save()
     res.send("User created successfully");
 } catch (error) {
     res.send("Error creating user: " + error.message);
 }
 });
+
+// Feed api - /feed - get all user from database
+app.get('/feed', async (req,res) => {
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch (error) {
+        res.status(400).send("something went wrong");
+    }
+})
 
 connectDB() // Connect to the database
     .then(() => {
